078093a3256ed3630c5b79011da34e17119bb7b8
Mark Boyle <markboyle19@gmail.com>
2021-07-13 16:19:55 +1000

    LOGIN COMPONENT: Added useState and a form to the LogIn component

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,5 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function LogIn(){
-    
+
+    const [loginForm, setLoginForm] = useState({
+        username: "",
+        password: ""
+    })
+
+    const changeInput = (event) => {
+        setLoginForm({
+            ...loginForm,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        
+    }
+
+    return(
+        <div>
+            Log In Page
+            <form onSubmit={onSubmit}>
+                <input type="text" name="username" id='username' placeholder="username" value ={loginForm.username} onChange={changeInput} />
+                <input type="password" name="password" placeholder="password" value ={loginForm.password} onChange={changeInput} />
+                <input type="submit"/>
+            </form>
+        </div>
+    )
 }
