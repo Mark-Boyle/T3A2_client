c6015450bf412deb20adaac8c3d4ce43867a4753
Mark Boyle <markboyle19@gmail.com>
2021-07-14 16:27:50 +1000

    Enabled Log In form to do a POST request to the Rails server

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 
-export default function LogIn(){
+export default function LogIn({history}){
 
     const [loginForm, setLoginForm] = useState({
         username: "",
         password: ""
     })
+
+    const [errorMessage, setErrorMessage] = useState("")
 
     const changeInput = (event) => {
         setLoginForm({
@@ -14,14 +16,35 @@ export default function LogIn(){
         })
     }
 
+    const sendLoginRequest = async () => {
+            const response = await fetch('http://localhost:3000/login', {      //Sending a POST request to the login method of the User controller in the Rails API.
+                method: 'POST',                                                  //The type of http request is POST.
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(loginForm)         //Sending the data in loginForm to the API in JSON format.
+            })
+    
+            const data = await response.json()              
+            console.log(data)
+            if(data.token){
+                localStorage.setItem('token', data.token)   //Storing the token received from the API in local storage.
+                    console.log("Success!!!")
+                    history.push('/')     //Redirecting to home page.
+            }else{
+                setErrorMessage(data.error)
+            }
+        }
+
     const onSubmit = (e) => {
         e.preventDefault();
-        
+        sendLoginRequest()
     }
 
     return(
         <div>
             Log In Page
+            {errorMessage && (<h5>{errorMessage}</h5>)} 
             <form onSubmit={onSubmit}>
                 <input type="text" name="username" id='username' placeholder="username" value ={loginForm.username} onChange={changeInput} />
                 <input type="password" name="password" placeholder="password" value ={loginForm.password} onChange={changeInput} />
