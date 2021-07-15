a0b62f184ab21e8cfa1c3e094a3732def495deeb
Mark Boyle <markboyle19@gmail.com>
2021-07-15 14:58:25 +1000

    Created sign out functionality

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, {useContext} from 'react';
 import { NavLink } from 'react-router-dom';
 import {AuthContext} from "../contexts/AuthProvider"
 
 export default function NavBar() {
+
+    const [auth, setAuth] = useContext(AuthContext);
+
+    const signOut = () => {
+        localStorage.removeItem('token')
+        localStorage.removeItem('username')
+        setAuth({
+            loggedIn: false,
+            username: null
+        })
+    }
     return (
         <nav>
-            <NavLink to='/'>Home</NavLink>
-            <NavLink to='/login'>Login</NavLink>
-            <NavLink to='/sign-up'>Sign Up</NavLink>
+`<NavLink to='/'>Home</NavLink>
+            {auth.loggedIn ? (
+                <>
+                    <h2>Logged in as: {auth.username}</h2>
+                    <div onClick={signOut}>Sign Out</div>
+                </>
+            ) : (
+                <>
+                    <NavLink to='/login'>Login</NavLink>
+                    <NavLink to='/sign-up'>Sign Up</NavLink>
+                </>
+            )}
         </nav>
     );
 }
